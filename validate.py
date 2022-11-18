24b40d05d5671f4c423f67ee5a96682b83447a32
geraldyeokj <geraldyeokj@gmail.com>
2022-11-18 14:50:47 -0600

    validate.py paste

--- validate.py
+++ validate.py
@@ -1,4 +1,7 @@
-ERROR_STRING = "ERROR: Invalid Address"
+import requests
+import json
+import os
+key = os.environ["geoapify_key"]
 
 def validate_addr(org, addr):
     '''
@@ -11,5 +14,43 @@ def validate_addr(org, addr):
     Outputs:
         result (string) - a valid address or ERROR_STRING
     '''
-    result = ERROR_STRING
-    return result
+    # key in your API key below 
+    geoapify_key = key
+
+    payload = {}
+    payload["apiKey"] = geoapify_key
+    payload["name"] = addr
+
+    response = requests.get("https://api.geoapify.com/v1/geocode/search?", params=payload)
+    response_j = response.json()
+    if response_j == {}:
+        print("No addresses found")
+        return "error"
+    properties = response_j["features"][0]["properties"]
+    
+    """
+    format of ret_str:
+    LINE 1: NAME OF ADDRESSEE
+    LINE 2: STREET ADDRESS OR POST OFFICE BOX NUMBER
+    LINE 3: CITY OR TOWN NAME, OTHER PRINCIPAL SUBDIVISION (i.e., PROVINCE, STATE, COUNTY, ETC.) AND POSTAL CODE (IF KNOWN)
+    (Note:  in some countries, the postal code may precede the city or town name)
+    LINE 4: COUNTRY NAME (UPPERCASE LETTERS IN ENGLISH)
+    """
+    
+    line_1 = org
+    line_2 = properties["address_line2"]
+    line_3 = properties["city"] + ", " +  properties["county"] + ", " +  properties["state"] + ", " +  properties["postcode"]
+    line_4 = properties["country"]
+
+    ret_str = line_1 + "\n" + line_2 + "\n" + line_3 + "\n" + line_4
+    
+    
+    #Use this if you want to see what your resposne looks like:
+    """
+    json_object = json.dumps(response.json(), indent=4)
+
+    with open("out3.json", "w") as outfile:
+        outfile.write(json_object)
+    """
+
+    return ret_str
