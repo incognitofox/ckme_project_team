44a52089a87e7099b7a5289ae62bbe12331d9743
geraldyeokj <geraldyeokj@gmail.com>
2023-02-19 00:16:56 -0600

    initial commit

---
+++ geocode.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import os
+from pycountry import countries
+import re
+from dotenv import load_dotenv
+
+
+#takes in an address string and returns a dict of longitude and latitude
+def geocode_addr(addr):
+    
+    load_dotenv()
+
+    key = os.getenv('GOOGLE_API_KEY')
+
+    ret_dict = {}
+
+    try: 
+
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+        data = {"key": key, "address": addr} 
+
+        r = requests.get(url, params=data)
+        print('success')
+
+        res = json.loads(r.text)
+
+        coordDict = res["results"][0]["geometry"]["location"]
+
+        #print(res)
+
+        ret_dict = coordDict
+
+    except:
+        print('failed')
+        pass
+
+    return ret_dict
+
+#print(geocode_addr("1156 E 61st street 60637"))
--- validate.py
+++ validate.py
@@ -3,14 +3,34 @@ import json
 import os
 from pycountry import countries
 import re
+from geocode import geocode_addr
+from googleAddressValidation import googleValidateAddressAPI
+from dotenv import load_dotenv
 
-key = os.environ["GOOGLE_API"]
+load_dotenv()
+
+key = os.getenv('GOOGLE_API_KEY')
 
 ERROR_STRING = "ERROR: invalid address"
 
+def address_simulator():
+    adr = {
+            'COMPANY': "CAPITAL CITY SYMPHONY",
+            'ADDRESS2': "",
+            'ADDRESS': "C/O ATLAS PERFORMING ARTS CENTER 1333 H STREET NE",
+            'CITY': "WASHINGTON",
+            'ZIP': "20002", 
+            'ST': "DC",
+            'COUNTRY': "United States"
+        }
+    return adr
+    
 def get_line_2(addr):
     line2 = ''
     for k in ['CITY', 'ST', 'ZIP','COUNTRY']:
+        print(addr[k])
+        if str(addr[k]).lower() == "nan" or str(addr[k]) == "":
+            continue
         val = addr[k]
         if isinstance(val, float):
             val = int(val)
@@ -19,7 +39,7 @@ def get_line_2(addr):
             line2 += val.strip() + ', '
     return line2[:-2]
 
-def validate_addr(addr):
+def validateWithTextsearch(addr):
     '''
     Returns a valid address given organization name and known address.
     If the address cannot be found, return an ERROR indicator
@@ -34,24 +54,63 @@ def validate_addr(addr):
     
     org = addr['COMPANY']
 
-    try: 
-        line2 = get_line_2(addr)
+    #try: 
+    line2 = get_line_2(addr)
 
-        url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+    url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
 
-        data = {"key": key, "query": f'{org} {line2}'} 
+    coords = geocode_addr(f'{org} {line2}')
 
-        r = requests.get(url, params=data)
-        print('success')
+    data = {"key": key, "query": f'{org} {line2}', "location": coords} 
 
-        res = json.loads(r.text)['results'][0]['formatted_address']
+    r = requests.get(url, params=data)
+    print('success')
 
-        print(res)
+    res = json.loads(r.text)['results'][0]['formatted_address']
 
-        ret_str = res
+    print(res)
 
-    except:
-        print('failed', org)
-        pass
+    ret_str = res
+
+    # except:
+    #     print('failed', org)
+    #     pass
 
     return ret_str
+
+
+def validate_addr(addr):
+    '''
+    Returns a valid address given organization name and known address.
+    If the address cannot be found, return an ERROR indicator
+
+    Inputs: 
+        org (string) - the organization name
+        addr (string) - string representation of address to validate
+    Outputs:
+        result (string) - a valid address or ERROR_STRING
+    '''
+    # ret_str = ERROR_STRING
+    
+    # org = addr['COMPANY']
+
+    # line2 = get_line_2(addr)
+
+    # addressQuery = f'{org} {line2}'
+
+    method1 = googleValidateAddressAPI(addr)
+
+    if method1 != ERROR_STRING:
+        print("method 1 used")
+        return method1
+    
+    method2 = validateWithTextsearch(addr)
+
+    if method2 != ERROR_STRING:
+        print("method 2 used")
+        return method2
+
+    print("Both methods failed")
+    return ERROR_STRING
+
+#print(validate_addr(address_simulator()))
